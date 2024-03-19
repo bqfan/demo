0eb7d947cfd19355dbfab8cb7151db056599f7b8
Baiquan Fan <baiquan.fan@gmail.com>
2024-03-19 16:33:51 +0200

    add validation.

--- src/main/java/com/example/demo/run/RunController.java
+++ src/main/java/com/example/demo/run/RunController.java
@@ -3,6 +3,8 @@ package com.example.demo.run;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import jakarta.validation.Valid;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -48,14 +50,14 @@ public class RunController {
     // POST
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run) {
+    void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
     //PUT
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
