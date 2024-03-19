9e58c6e1d21d23b624cc966b53471bc952a35082
Baiquan Fan <baiquan.fan@gmail.com>
2024-03-19 20:36:10 +0200

    add initial database (h2) JDBC connection.

--- src/main/java/com/example/demo/run/RunRepository.java
+++ src/main/java/com/example/demo/run/RunRepository.java
@@ -1,60 +1,69 @@
 package com.example.demo.run;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
-
-import jakarta.annotation.PostConstruct;
+import com.example.demo.DemoApplication;
 
 @Repository
 public class RunRepository {
 
-    private List<Run> runs = new ArrayList<>();
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    private final JdbcClient jdbcClient;
 
-    List<Run> findAll() {
-        return runs;
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
-    Optional<Run> findById(Integer id) {
-        return runs.stream()
-        .filter(run -> run.id() == id)
-        .findFirst();
+    public List<Run> findAll() {
+        return jdbcClient.sql("select * from run")
+            .query(Run.class)
+            .list();
     }
+    // private List<Run> runs = new ArrayList<>();
 
-    void create(Run run) {
-        runs.add(run);
-    }
+    // List<Run> findAll() {
+    //     return runs;
+    // }
 
-    void update(Run run , Integer id) {
-        Optional<Run> existingRun = findById(id);
+    // Optional<Run> findById(Integer id) {
+    //     return runs.stream()
+    //     .filter(run -> run.id() == id)
+    //     .findFirst();
+    // }
 
-        if (existingRun.isPresent()) {
-            runs.set(runs.indexOf(existingRun.get()), run);
-        }
-    }
+    // void create(Run run) {
+    //     runs.add(run);
+    // }
 
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
-    }
+    // void update(Run run , Integer id) {
+    //     Optional<Run> existingRun = findById(id);
 
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(
-            1, "Monday Morning Run",
-            LocalDateTime.now(),
-            LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
-            3,
-            Location.INDOOR));
+    //     if (existingRun.isPresent()) {
+    //         runs.set(runs.indexOf(existingRun.get()), run);
+    //     }
+    // }
 
-        runs.add(new Run(
-            2, "Wednesday Evening Run",
-            LocalDateTime.now(),
-            LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
-            6,
-            Location.INDOOR));
-    }
+    // void delete(Integer id) {
+    //     runs.removeIf(run -> run.id().equals(id));
+    // }
+
+    // @PostConstruct
+    // private void init() {
+    //     runs.add(new Run(
+    //         1, "Monday Morning Run",
+    //         LocalDateTime.now(),
+    //         LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+    //         3,
+    //         Location.INDOOR));
+
+    //     runs.add(new Run(
+    //         2, "Wednesday Evening Run",
+    //         LocalDateTime.now(),
+    //         LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+    //         6,
+    //         Location.INDOOR));
+    // }
 }
