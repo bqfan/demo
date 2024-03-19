6b11cf67479b642fb767f42c92149a0aacf7cf60
Baiquan Fan <baiquan.fan@gmail.com>
2024-03-19 20:53:09 +0200

    restore post, update and delete actions.

--- src/main/java/com/example/demo/run/RunController.java
+++ src/main/java/com/example/demo/run/RunController.java
@@ -2,10 +2,20 @@ package com.example.demo.run;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import jakarta.validation.Valid;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 
@@ -25,34 +35,34 @@ public class RunController {
         return runRepository.findAll();
     }
 
-    // @GetMapping("/{id}")
-    // Run findById(@PathVariable Integer id) {
-    //     Optional<Run> run = runRepository.findById(id);
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        Optional<Run> run = runRepository.findById(id);
 
-    //     if (!run.isPresent()) {
-    //         throw new RunNotFoundException();
-    //     };
+        if (!run.isPresent()) {
+            throw new RunNotFoundException();
+        };
 
-    //     return run.get();
-    // }
+        return run.get();
+    }
 
-    // // POST
-    // @ResponseStatus(HttpStatus.CREATED)
-    // @PostMapping("")
-    // void create(@Valid @RequestBody Run run) {
-    //     runRepository.create(run);
-    // }
+    // POST
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@Valid @RequestBody Run run) {
+        runRepository.create(run);
+    }
 
-    // //PUT
-    // @ResponseStatus(HttpStatus.NO_CONTENT)
-    // @PutMapping("/{id}")
-    // void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
-    //     runRepository.update(run, id);
-    // }
+    //PUT
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+    }
 
-    // @ResponseStatus(HttpStatus.NO_CONTENT)
-    // @DeleteMapping("/{id}")
-    // void delete(@PathVariable Integer id) {
-    //     runRepository.delete(id);
-    // }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
+    }
 }
